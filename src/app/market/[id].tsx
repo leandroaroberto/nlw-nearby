9d2fc34e18aca17297c100dd827ce40ad6aa9a04
Leandro Roberto <Leandro.Roberto.Contractor@pepsico.com>
2024-12-15 19:48:32 -0300

    Market component

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,13 +1,85 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router"
+import { useEffect, useState } from "react";
+import { View, Alert, Modal } from "react-native";
+import { router, useLocalSearchParams, Redirect } from "expo-router"
+import { api } from "@/services/api";
 
+import { Loading } from "@/components/loading";
+import { Cover } from "@/components/market/cover";
+
+import { Details, PropsDetails } from "@/components/market/details";
+import { Coupon } from "@/components/market/coupon";
+import { Button } from "@/components/button";
+
+type DataProps = PropsDetails & {
+    cover: string
+}
 
 export default function Market() {
+    const [ data, setData ] = useState<DataProps>()
+    const [ coupon, setCoupon ] = useState<string | null>(null)
+    const [ isVisibleCameraModal, setIsVisibleCameraModal ] = useState(false)
+
+    const [ isLoading, setIsLoading ] = useState(true)
+
     const params = useLocalSearchParams< { id: string } >()
     
+
+    async function fetchMarket() {
+        try {
+            const { data } = await api.get(`/markets/${params.id}`)
+            setData(data)
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Erro", "Não foi possível carregar os dados.", [{
+                text: "OK",
+                onPress: () => router.back(),
+            }])
+        }
+    }
+
+    useEffect(() => {
+        fetchMarket()
+    },[params.id])
+
+    function handleOpenCamera() {
+        try {   
+           setIsVisibleCameraModal(true) 
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    if (! data) {
+        return <Redirect href="/home"/>
+    }
+
     return (
-        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-            <Text>{params.id}</Text>
+        <View style={{flex: 1}}>
+            <Cover uri={data.cover}/>
+            <Details data={data}/>
+            
+            {coupon && <Coupon code={coupon}/>}
+
+            <View style={{padding: 32}}>
+                <Button onPress={handleOpenCamera}>
+                    <Button.Title>Ler QR Code</Button.Title>
+                </Button>
+            </View>
+
+            <Modal style={{ flex: 1}} visible={isVisibleCameraModal}>
+                <View style={{flex: 1, justifyContent: "center"}}>
+                    <Button onPress={() => setIsVisibleCameraModal(false)}>
+                        <Button.Title>
+                            Voltar
+                        </Button.Title>
+                    </Button>
+                </View>
+            </Modal>
         </View>
     )
 }
